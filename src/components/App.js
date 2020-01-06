545f403d674fe9c9814ed0f757522ef1cb6912e6
nickhuang <sef1548@gmail.com>
2020-01-07 01:37:32 +0800

    test

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,21 @@ import Title from './Title';
 import { connect } from 'react-redux';
 import Preview from './Preview';
 import Editor from './Editor';
-// import Header from './Header';
 import {
     Switch,
     Route,
     Link
 } from 'react-router-dom';
-
 import '../style/App.css';
 
 function App() {
     return (
         <>
         <Switch>
-                    <Route exact path={ [`/:id`, `/new`] }>
-                        <Link to={ `/` }>HOME</Link>
-                    </Route>
-                </Switch>
+            <Route exact path={ [`/:id`, `/new`] }>
+                <Link to={ `/` }>HOME</Link>
+            </Route>
+        </Switch>
         <article className="App">
            
             <Title />
